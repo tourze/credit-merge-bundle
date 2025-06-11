7343391049142d9968bf741cbb2665cd0d9d0092
github-ci <you@example.com>
2025-06-12 05:53:49 +0800

    fix catch

--- src/Service/CreditMergeService.php
+++ src/Service/CreditMergeService.php
@@ -67,7 +67,7 @@ class CreditMergeService
 
             return $mergeCount;
 
-        } catch  (\Throwable $e) {
+        } catch (\Throwable $e) {
             $this->entityManager->getConnection()->rollBack();
 
             $this->logger->error('积分合并失败', [
